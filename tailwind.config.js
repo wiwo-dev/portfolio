deb36b44d4338ab6ff9f93f44a5591a304483226
wiwo-one <wojtekwieclawski@gmail.com>
2022-10-26 12:35:26 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    // fontFamily: {
+    //   sans: ["Graphik", "sans-serif"],
+    //   serif: ["Merriweather", "serif"],
+    // },
+    extend: {},
+  },
+  plugins: [],
+};
