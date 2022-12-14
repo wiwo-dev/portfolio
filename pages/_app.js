adc8ed7da77d8e3036b8b233c9d98d9d5c46d8d4
wiwo-one <wojtekwieclawski@gmail.com>
2022-12-14 14:27:48 +0530

    seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        title="Wojciech Wieclawski"
+        description="Web Developer with corporate experiance"
+        openGraph={{
+          url: "https://wiwoproduction.com",
+          title: "Wojciech Wieclawski",
+          description: "Web Developer with corporate experiance",
+          images: [
+            {
+              url: "https://staging.wiwoproduction.com/seo/ogimage.jpg",
+              width: 1200,
+              height: 627,
+              alt: "Wojciech Wieclawski | wiwoproduction.com",
+              type: "image/jpeg",
+            },
+          ],
+          siteName: "wiwoproduction.com",
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
