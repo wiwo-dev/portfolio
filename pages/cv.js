c1987482d3c33615f4b01168f4460373617d0cd9
wiwo-one <wojtekwieclawski@gmail.com>
2023-01-13 13:14:02 +0530

    cv update

--- pages/cv.js
+++ pages/cv.js
@@ -53,109 +53,6 @@ const data = {
       },
     ],
   },
-  experience: {
-    intro:
-      "I’m a developer with many years of experience in the corporate world of Digital Marketing, Social Media, and Video Production. I’m a creative mind with plenty of experience in the digital world. In my work, I strive to create thoughtful, user-centred experiences that adhere to web standards. I build websites and web applications using JavaScript, React and NextJS. In order to “make it pop” I use Framer Motion for animations. ",
-    jobs: [
-      {
-        company: "Freelance",
-        position: "Web Developer, Social Media Strategist",
-        time: "Aug 2022 - Present",
-        description: {
-          intro:
-            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta quasi accusantium voluptas saepe facere hic",
-          bulletpoints: [
-            "Building web pages and web applications using React and Webflow",
-            "Developing social media strategy, creative content ideas, and conducting workshops and trainings.",
-          ],
-        },
-      },
-      {
-        company: "Red Bull",
-        position: "Social Media Manager",
-        time: "Jan 2019 - Apr 2022",
-        location: "Warsaw, Poland",
-        description: {
-          intro:
-            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta quasi accusantium voluptas saepe facere hic",
-          bulletpoints: [
-            "Managing a team responsible for over 20 social media profiles (Facebook, Instagram, YouTube, TikTok, Discord)",
-            "Content creation and curation across all channels and ensuring a cohesive online brand identity.",
-            "Tracking and analyzing data to ensure a high engagement rate and driving users to owned channels.",
-            "Planning and executing paid advertising. Preparing media plans, campaign optimization, and ongoing control of expenses. Depending on the campaign: from working with a media agency to working independently in Facebook’s advertising system.",
-            "Defining and developing social content creators and opinion leaders programs. Identifying and hiring new creators. Building long-term relationships.",
-            "Digital projects management. Building and developing websites, platforms and digital experiences on redbull.com",
-          ],
-          projects: [
-            "Red Bull Enduro Liga - A platform that let us move Enduro-Motocross competitions to virtual world during the pandemic. Riders were uploading their video submitions throughtout 6 weeks period. Each entry was checked and assesed by the admins. Platform was connected to Discord server.",
-          ],
-        },
-      },
-      {
-        company: "Red Bull",
-        position: "Digital Specialist",
-        time: "Jun 2016 - Jan 2019",
-        location: "Warsaw, Poland",
-        description: {
-          intro:
-            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta quasi accusantium voluptas saepe facere hic",
-          bulletpoints: [
-            "Concept development and supervision of website creation for Red Bull projects.",
-            "Developing, creating and implementing a communication plan in owned and managed media channels.",
-            "Creating video content for social media and www. Depending on the project: cooperation with production teams or independent recording and editing.",
-            "Support and implementation of tools extending the capabilities of redbull.com",
-          ],
-          projects: [
-            "Red Bull Wjazd na Bramkę - Online platform for esport tournament in Rocket League. Team pairing, bracket generation, results submition and live result view during final stage of the project.",
-            "Digital Activation for Red Bull Tour Bus music project. Users were asked to translate song lyrics to emojis and submit their creative work to the contest. App was generating graphics that were easilly shareble in social medias.",
-          ],
-        },
-      },
-      {
-        company: "Red Bull",
-        position: "Digital Marketing Trainee",
-        time: "Sep 2015 - Jun 2016",
-        location: "Warsaw, Poland",
-        description: {
-          intro:
-            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta quasi accusantium voluptas saepe facere hic",
-          bulletpoints: [
-            "Preparation of content plans and ideas for activations for managed and owned channels.",
-            "Managing social media accounts. Ongoing community management, replying to messages and comments, creating reports and analysis.",
-            "Creating and supervising contests conducted on social media and on redbull.com.",
-            "Cooperation with other company departments, interactive agencies and external partners in digital activities related to the creation of event pages and other digital activations of Red Bull.",
-          ],
-        },
-      },
-      {
-        company: "Red Bull",
-        position: "Student Brand Manager",
-        time: "Apr 2012 - Jun 2015",
-        location: "Poznan, Poland",
-        description: {
-          intro:
-            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta quasi accusantium voluptas saepe facere hic",
-          bulletpoints: [
-            "Brand ambassador working at Poznań University of Technology.",
-            "Cooperation with student organizations in the city and supporting their initiatives.",
-            "Organizing events for students.",
-            "Maintaining good relations with local media (radio stations, TVs, newspapers).",
-          ],
-        },
-      },
-      {
-        company: "Kitesurfing, Windsurfing and Skiing Instructor",
-        position: "",
-        location: "Poland, Greece, Brazil, Austria, France, Italy and more...",
-        time: "2010 - Sep 2015",
-        description: {
-          intro:
-            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta quasi accusantium voluptas saepe facere hic",
-          bulletpoints: ["Group and individual lessons, mainly for adults. Teaching in Polish and English."],
-        },
-      },
-    ],
-  },
 };
 
 export default function cv() {
@@ -189,7 +86,7 @@ export default function cv() {
             </div>
             <ul className="hidden md:block w-[80%]">
               <li className="flex items-end gap-3">
-                <span className="w-[20px] flex justify-center">
+                <span className="min-w-[20px] flex justify-center">
                   <MailIcon />
                 </span>
                 <a className="underline" href="mailto:wojtekwieclawski@gmail.com?subject=Hello!">
@@ -197,15 +94,15 @@ export default function cv() {
                 </a>
               </li>
               <li className="flex items-end gap-3">
-                <span className="w-[20px] flex justify-center">
+                <span className="min-w-[20px] flex justify-center">
                   <WebIcon />
                 </span>
                 <span className="underline">
                   <Link href="https://wiwo.dev">https://wiwo.dev</Link>
                 </span>
               </li>
-              <li className="flex items-end gap-3">
-                <span className="w-[20px] flex justify-center">
+              <li className="flex items-center gap-3">
+                <span className="min-w-[20px] flex justify-center">
                   <GithubIcon />
                 </span>
                 <span className="underline">
@@ -213,7 +110,7 @@ export default function cv() {
                 </span>
               </li>
               <li className="flex items-center gap-3 leading-5 unde">
-                <span className="w-[26px] flex justify-center">
+                <span className="min-w-[20px] flex justify-center">
                   <LinkedInIcon width={14} />
                 </span>
                 <span className="underline">
@@ -238,7 +135,7 @@ export default function cv() {
               </ul>
             </div>
 
-            <div className="hidden md:block w-[80%]">
+            <div className="hidden w-[80%]">
               <h1 className="text-black text-base lg:text-lg font-bold">
                 <CVHeadingUnderline>Education</CVHeadingUnderline>
               </h1>
@@ -272,24 +169,26 @@ export default function cv() {
           </div>
           <div className="md:w-2/3 mt-8 md:mt-0">
             <div className="flex flex-col gap-5">
-              <CVTitle>Wojciech Więcławski</CVTitle>
+              <h1 className="text-3xl lg:text-4xl text-black font-extrabold">Wojciech Więcławski</h1>
               <p className="text-xl">Web Developer with digital marketing experience</p>
               <p className="md:pr-[20%] pb-5">{data.heading.intro}</p>
             </div>
             <hr className="w-2/3"></hr>
-            <div className="flex flex-col gap-5 py-4 lg:pr-16 md:hidden">
+            <div className="flex flex-col gap-5 py-4 lg:pr-16">
               <span>
                 <CVHeadingUnderline>Education</CVHeadingUnderline>
               </span>
               {data.education.schools.map((el, ind) => (
-                <CVPosition
-                  key={ind}
-                  variant="two-lines"
-                  headline={el.school}
-                  subheadline={`${el.field} (${el.level})`}
-                  time={el.time}
-                  location={el.location}
-                />
+                <div key={ind}>
+                  <div className="flex flex-col md:flex-row justify-between md:gap-10">
+                    <p className="text-lg">
+                      <span className="font-bold">{el.eschool}</span>
+                      <span>{`${el.field} (${el.level})`}</span>
+                    </p>
+                    <p className="flex-none text-sm lg:text-base">{el.time}</p>
+                  </div>
+                  <div className="text-base opacity-60">{el.location}</div>
+                </div>
               ))}
             </div>
             <div className="flex flex-col gap-5 py-4 mt-1 lg:pr-16">
@@ -312,7 +211,10 @@ export default function cv() {
                   </ul>
                   <p className="pt-1 md:pr-16 pl-5">
                     Description and source code is available at my portfolio website (NextJS, GraphQL, Contentful,
-                    Framer Motion)
+                    Framer Motion){" "}
+                    <span className="underline">
+                      <Link href="https://wiwo.dev">https://wiwo.dev</Link>
+                    </span>
                   </p>
                 </div>
               </div>
@@ -403,15 +305,6 @@ export default function cv() {
   );
 }
 
-function CVHeading({ variant = "h1", className, children }) {
-  const underlineH1 = "inline-block shadow-[inset_0_-0.45em_0_0_#FCD13A]";
-  if (variant === "h2") {
-    return <h2 className={`text-black text-lg lg:text-xl font-bold ${className}`}>{children}</h2>;
-  } else {
-    return <h1 className={`text-black text-lg lg:text-xl font-bold ${underlineH1} ${className}`}>{children}</h1>;
-  }
-}
-
 function CVHeadingUnderline({ children }) {
   return (
     <span>
@@ -420,10 +313,6 @@ function CVHeadingUnderline({ children }) {
       </h1>
     </span>
   );
-}
-
-function CVTitle({ children }) {
-  return <h1 className="text-3xl lg:text-4xl text-black font-extrabold">{children}</h1>;
 }
 
 function CVPosition({ headline, subheadline, time, variant = "one-line", location = "", body, children }) {
