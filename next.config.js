a63598f83df6f80cdfadcc9658894dbdf639fecb
wiwo-one <wojtekwieclawski@gmail.com>
2024-01-02 11:59:01 +0800

    resume

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
   images: {
     domains: ["images.ctfassets.net"],
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/resume",
+        destination: "/Resume-Wojciech-Wieclawski.pdf",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
